a42aec1ba8e1badb2ce4a089c3efe3313dac1ae5
Memory Byte <pratikpsingh25@gmail.com>
2025-12-05 03:52:21 +0530

    Structure Outputs - TypedDict, Pydantic, JSON, When to use what

---
+++ 03-structured-output/02_pydantic_with_structured_output.py
@@ -0,0 +1,60 @@
+from pprint import pprint
+from dotenv import load_dotenv
+from typing import TypedDict, Annotated, Optional, Literal
+from pydantic import BaseModel, Field
+from langchain_openai import ChatOpenAI
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
+from langchain_core.prompts import ChatPromptTemplate
+
+load_dotenv()
+
+# model = ChatOpenAI(model='gpt-4.1-nano')
+model = ChatGoogleGenerativeAI(model='gemini-2.5-flash')
+
+# Schema
+# class Review(TypedDict):
+#     summary: str
+#     sentiment: str
+
+class Review(BaseModel):
+    key_themese: list[str] = Field(description='List down all the key themes discussed in the review in a list')
+    summary: str = Field(description='A brief summary of the review')
+    sentiment: Literal['positive', 'negative', 'neutral'] = Field(description='Return sentiment of the review. Either negative, positive or neutral')
+    pros: Optional[list[str]] = Field(default=None, description='Write down all the pros inside a list')
+    cons: Optional[list[str]] = Field(default=None, description='Write the name of the reviewer')
+    name: Optional[str] = Field(default=None, description='Write the name of the reviewer')
+
+
+model_with_structured_output = model.with_structured_output(Review)
+
+# prompt = (
+#     'The hardware is great, but the software feels bloated. '
+#     'There are too many pre-installed apps that I can not remove. '
+#     'Also, the UI looks outdated compared to other brands. Hoping for a software update to fix this.'
+#     )
+
+prompt = (
+    'I recently upgraded to the Samsung Galaxy S24 Ultra, and I must say, it is an absolute powerhouse!' 
+    'The Snapdragon 8 Gen 3 processor makes everything lightning fast—whether I am gaming, multitasking, or editing photos.'
+    'The 5000mAh battery easily lasts a full day even with heavy use, and the 45W fast charging is a lifesaver.'
+    'The S-Pen integration is a great touch for note-taking and quick sketches, though I donot use it often.'
+    'What really blew me away is the 200MP camera—the night mode is stunning, capturing crisp, vibrant images even in low light.'
+    'Zooming up to 100x actually works well for distant objects, but anything beyond 30x loses quality.'
+    'However, the weight and size make it a bit uncomfortable for one-handed use.' 
+    'Also, Samsungs One UI still comes with bloatware—why do I need five different Samsung apps for things Google already provides?' 
+    'The $1,300 price tag is also a hard pill to swallow.'
+    'Pros:'
+    'Insanely powerful processor (great for gaming and productivity)'
+    'Stunning 200MP camera with incredible zoom capabilities'
+    'Long battery life with fast charging'
+    'S-Pen support is unique and useful'
+
+    'Review by Memory Byte'
+)
+
+result = model_with_structured_output.invoke(prompt)
+
+pprint(result)
+print('\n\n')
+print(type(result))
